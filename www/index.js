52b8b96eaa1f8fbf0e0551b053c2f4bd95728c12
Ealden Esto E. Escanan <ealden@gmail.com>
2012-01-01 05:52:53 +0800

    Initial listing of events for a given user
    We only display PushEvents, and log other events to the console.
    The Activity Stream page would need more UI polish to properly
    display the different events GitHub generates.
    
    Signed-off-by: Ealden Esto E. Escanan <ealden@gmail.com>

--- www/index.js
+++ www/index.js
@@ -1,13 +1,5 @@
-var loaded = false;
-
-$('#main-page').live('pageshow', show_progress_loader);
-$('#main-page').live('pageinit', load_repos);
-
-function show_progress_loader() {
-    if (!loaded) {
-        $.mobile.showPageLoadingMsg();
-    }
-}
+$('#events-page').live('pageinit', load_events);
+$('#repos-page').live('pageinit', load_repos);
 
 function load_repos() {
     $.getJSON('https://api.github.com/users/ealden/repos?callback=?', function(data) {
@@ -18,8 +10,20 @@ function load_repos() {
         });
 
         $('#repo-list ul').listview('refresh');
+    });
+}
 
-        $.mobile.hidePageLoadingMsg();
-        loaded = true;
+function load_events() {
+    $.getJSON('https://api.github.com/users/ealden/received_events?callback=?', function(data) {
+        $.each(data.data, function(index, event) {
+            if (event.type == 'PushEvent') {
+                $('#events-list ul').append(
+                    $('<li>').append(event.actor.login));
+            } else {
+                console.log(event.type);
+            }
+        });
+
+        $('#events-list ul').listview('refresh');
     });
 }
