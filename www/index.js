29c5120ffa0a977a782acc664d56e0d311dc87f7
Ealden Esto E. Escanan <ealden@gmail.com>
2012-01-01 13:23:44 +0800

    Display commits on each push event
    Signed-off-by: Ealden Esto E. Escanan <ealden@gmail.com>

--- www/index.js
+++ www/index.js
@@ -1,8 +1,13 @@
 $('#events-page').live('pageinit', load_events);
 $('#repos-page').live('pageinit', load_repos);
 
+$('#events-refresh').live('click', load_events);
+$('#repos-refresh').live('click', load_repos);
+
 function load_repos() {
     $.getJSON('https://api.github.com/users/ealden/repos?callback=?', function(data) {
+        $('#repo-list ul').empty();
+
         $.each(data.data, function(index, repo) {
             $('#repo-list ul').append(
                 $('<li>').append(
@@ -15,10 +20,11 @@ function load_repos() {
 
 function load_events() {
     $.getJSON('https://api.github.com/users/ealden/received_events?callback=?', function(data) {
+        $('#events-list ul').empty();
+
         $.each(data.data, function(index, event) {
             if (event.type == 'PushEvent') {
-                $('#events-list ul').append(
-                    $('<li>').append(event.actor.login));
+                push_event(event);
             } else {
                 console.log(event.type);
             }
@@ -26,4 +32,23 @@ function load_events() {
 
         $('#events-list ul').listview('refresh');
     });
+}
+
+function push_event(event) {
+    var item = $('<li>');
+
+    item.append($('<h3>')
+                .append(event.actor.login)
+                .append(' pushed to ')
+                .append(event.payload.ref)
+                .append(' on ')
+                .append(event.repo.name));
+
+    $.each(event.payload.commits, function(commit_index, commit) {
+        var sha = $('<code>').append(commit.sha.substring(0, 8));
+
+        item.append($('<p>').append(sha).append(' ').append(commit.message));
+    });
+
+    $('#events-list ul').append(item);
 }
